b2a28459f0ed768303ca866eb305d6647f98efc7
Xayias <Xayias1@yahoo.com>
2025-02-27 20:53:34 -0600

    Made some fixes to the responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,13 +9,6 @@ const Navbar = () => {
         <nav className="bg-black text-white py-4 fixed w-full z-50">
             <div className="container mx-auto flex justify-between items-center px-4">
 
-                {/* Logo in Center (on Desktop) */}
-                <div className="hidden md:flex flex-grow justify-center">
-                  <Link to="/">
-                    <img src="/psretro/psretrologo-full.webp" alt="PS Retro Full Logo" className="h-auto w-28" />
-                  </Link>
-                </div>
-
                 {/* Hamburger Menu Button (Only on Mobile) */}
                 <button className="md:hidden text-white text-3xl z-50" onClick={() => setMenuOpen(!menuOpen)}>
                     <FaBars />
@@ -34,6 +27,12 @@ const Navbar = () => {
                     <li>
                         <Link to="/psp" className="hover:text-gray-400 text-4xl">PSP</Link>
                     </li>
+                    {/* Logo in Center (on Desktop) */}
+                    <div className="hidden md:flex flex-grow justify-center">
+                      <Link to="/">
+                        <img src="/psretro/psretrologo-full.webp" alt="PS Retro Full Logo" className="h-auto w-28" />
+                      </Link>
+                    </div>
                     <li>
                         <Link to="/ps-vita" className="hover:text-gray-400 text-4xl">PS Vita</Link>
                     </li>
@@ -50,21 +49,17 @@ const Navbar = () => {
 
                 {/* Mobile Fullscreen Menu */}
                 <div className={`fixed top-0 left-0 w-full h-screen bg-black flex flex-col items-center justify-center transition-transform duration-300 ${menuOpen ? "translate-x-0" : "-translate-x-full"} md:hidden`}>
-                    {/* Close Icon Inside Menu */}
-                    <button className="absolute top-6 right-6 text-white text-4xl" onClick={() => setMenuOpen(false)}>
-                      <FaTimes />
-                    </button>
-
                     {/* Mobile Menu Links */}
                     <ul className="flex flex-col items-center gap-8 text-3xl font-semibold">
-                      <li><Link to="/ps1" className="hover:text-gray-400" onClick={() => setMenuOpen(false)}>PS1</Link></li>
-                      <li><Link to="/ps2" className="hover:text-gray-400" onClick={() => setMenuOpen(false)}>PS2</Link></li>
-                      <li><Link to="/ps3" className="hover:text-gray-400" onClick={() => setMenuOpen(false)}>PS3</Link></li>
-                      <li><Link to="/psp" className="hover:text-gray-400" onClick={() => setMenuOpen(false)}>PSP</Link></li>
-                      <li><Link to="/ps-vita" className="hover:text-gray-400" onClick={() => setMenuOpen(false)}>PS Vita</Link></li>
-                      <li><Link to="/ps4" className="hover:text-gray-400" onClick={() => setMenuOpen(false)}>PS4</Link></li>
-                      <li><Link to="/ps-vr" className="hover:text-gray-400" onClick={() => setMenuOpen(false)}>PS VR</Link></li>
-                      <li><Link to="/ps5" className="hover:text-gray-400" onClick={() => setMenuOpen(false)}>PS5</Link></li>
+                        <li><Link to="/"><img src="/psretro/psretrologo-full.webp" alt="PS Retro Full Logo" className="h-auto w-28" /></Link></li>
+                        <li><Link to="/ps1" className="hover:text-gray-400" onClick={() => setMenuOpen(false)}>PS1</Link></li>
+                        <li><Link to="/ps2" className="hover:text-gray-400" onClick={() => setMenuOpen(false)}>PS2</Link></li>
+                        <li><Link to="/ps3" className="hover:text-gray-400" onClick={() => setMenuOpen(false)}>PS3</Link></li>
+                        <li><Link to="/psp" className="hover:text-gray-400" onClick={() => setMenuOpen(false)}>PSP</Link></li>
+                        <li><Link to="/ps-vita" className="hover:text-gray-400" onClick={() => setMenuOpen(false)}>PS Vita</Link></li>
+                        <li><Link to="/ps4" className="hover:text-gray-400" onClick={() => setMenuOpen(false)}>PS4</Link></li>
+                        <li><Link to="/ps-vr" className="hover:text-gray-400" onClick={() => setMenuOpen(false)}>PS VR</Link></li>
+                        <li><Link to="/ps5" className="hover:text-gray-400" onClick={() => setMenuOpen(false)}>PS5</Link></li>
                     </ul>
                 </div>
             </div>
